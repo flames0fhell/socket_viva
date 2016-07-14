2ae5a80df019d1af06cd2819f208833e449c7b3a
hitori <hito.gomail@gmail.com>
2016-07-14 16:02:57 +0700

    https

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+var fs = require('fs');
+
+var privateKey = fs.readFileSync('hitoriaf.com-cert/hitoriafcom.key');
+var certificate = fs.readFileSync('hitoriaf.com-cert/hitoriafcom.crt');
+
+var credentials = {key: privateKey, cert: certificate};
+
+var app = express.createServer(credentials);
 
 app.get('/', function(req, res){
   res.send('Keep Calm, Its Active');
